f1ebda14bdef2c2ee83ebc39c914874bc5ccf5b9
kiseokkm <311kskim@gmail.com>
2024-07-10 14:48:25 +0900

    feat:init

---
+++ src/main/java/com/sparta/sensorypeople/SensoryPeopleApplication.java
@@ -0,0 +1,15 @@
+package com.sparta.sensorypeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SensoryPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoryPeopleApplication.class, args);
+	}
+
+}
